3d60bac37c7c4118d9dd2bcb7754676c4e4bdd16
Anis Ben Amara <anis.ben.amara89@gmail.com>
2023-02-11 15:28:34 +0100

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: [],
-  purge: {
-    enabled: process.env.NODE_EN === 'production',
-    content: ['src/components/**/*.tsx', 'src/pages/**/*.tsx'],
-  },
+  content: ['src/components/**/*.tsx', 'src/pages/**/*.tsx'],
 };
