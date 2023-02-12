82918790a65364670be5af5078254c1fe807d428
Anis Ben Amara <anis.ben.amara89@gmail.com>
2023-02-12 23:59:08 +0100

    Active navlink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,26 @@ import { useTheme } from 'next-themes';
 import Navigation from '../Navigation';
 
 const Header = () => {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
+
+  React.useEffect(() => {
+    setTheme(systemTheme === 'dark' ? 'dark' : 'light');
+  }, [systemTheme, setTheme]);
+
+  function toggleTheme(): void {
+    const isDarkTheme = theme === 'dark';
+    setTheme(isDarkTheme ? 'light' : 'dark');
+  }
 
   return (
-    <header className="flex items-center justify-start gap-2 px-4 py-5 bg-primary text-slate-50">
+    <header className="flex items-center justify-start gap-2 px-4 bg-primary text-slate-50">
       <h1 className="text-3xl font-bold">Anis</h1>
       <Navigation />
       <button
         className="h-8 p-2 rounded-full w-fit bg-secondary text-slate-50 border-neutral-50"
-        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        onClick={toggleTheme}
       >
-        Toggle Me ({theme})
+        Toggle Me [{theme}]
       </button>
     </header>
   );
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,16 +1,42 @@
 import * as React from 'react';
 
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+type NavLinkProps = {
+  path: string;
+  title: string;
+};
+
+const NavLink = (props: NavLinkProps) => {
+  const { title, path } = props;
+
+  const router = useRouter();
+
+  const activeLinkCSS = 'border-b-white border-b-2 border-solid';
+
+  function activeLink(path: string): string {
+    if (path === '/') {
+      return router.pathname === path ? activeLinkCSS : '';
+    }
+    return router.pathname.startsWith(path) ? activeLinkCSS : '';
+  }
+
+  return (
+    <Link
+      href={path}
+      className={`${activeLink(path)} w-16 text-center py-4 inline-block`}
+    >
+      {title}
+    </Link>
+  );
+};
 
 const Navigation = () => {
   return (
     <nav className="flex gap-2 ml-auto">
-      <Link className="" href="/">
-        Home
-      </Link>
-      <Link className="" href="/blog">
-        Blog
-      </Link>
+      <NavLink path="/" title="Home" />
+      <NavLink path="/blog" title="Blog" />
     </nav>
   );
 };
