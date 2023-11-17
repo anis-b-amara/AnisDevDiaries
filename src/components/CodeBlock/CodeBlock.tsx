180e332d357ce027d42507a21b56f0a8be6a7e69
Anis Ben Amara <anis.ben.amara89@gmail.com>
2023-11-17 22:33:04 +0100

    Add Copy on clipboard

--- src/components/CodeBlock/CodeBlock.tsx
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 import Prism from 'prismjs';
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-css';
@@ -9,6 +11,7 @@ import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-cshtml';
 
 import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/plugins/toolbar/prism-toolbar.css';
 
 interface CodeBlockProps {
   children: string;
@@ -18,11 +21,15 @@ interface CodeBlockProps {
 const CodeBlock = (props: CodeBlockProps): JSX.Element => {
   const { children, language } = props;
 
-  React.useEffect(() => Prism.highlightAll(), []);
+  React.useEffect(() => {
+    Prism.highlightAll();
+  }, []);
 
   return (
     <pre>
-      <code className={`language-${language}`}>{children}</code>
+      <code className={`language-${language}`} data-prismjs-copy="Copy">
+        {children}
+      </code>
     </pre>
   );
 };
